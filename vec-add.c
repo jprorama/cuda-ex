9b482d93af65d18ae1b9e58ec9e54ca90b115c19
John-Paul Robinson <jprorama@gmail.com>
2017-06-01 04:05:54 -0500

    Add minimal supporting code to compile vector add example
    Add header files, array init and print functions for inspection
    Add input args to size the vector
    Add output alloc and inspection code

--- vec-add.c
+++ vec-add.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 // Device code
 __global__ void VecAdd(float* A, float* B, float* C, int N)
 {
@@ -5,19 +8,42 @@ __global__ void VecAdd(float* A, float* B, float* C, int N)
   if (i < N)
     C[i] = A[i] + B[i];
 }
-            
+  
+float *initarray(float *a, int N, float value) {
+  int i;
+
+  for (i=0; i<N; i++)
+      a[i] = drand48()*value;
+
+  return a;
+}
+
+void printarray(float *a, int N) {
+  int i;
+
+  for (i=0; i<N; i++) {
+      printf("%d ", a[i]);
+    printf("\n");
+  }
+}
+          
 // Host code
-int main()
+int main(int argc, char **argv)
 {
-  int N = ...;
+  int N = atoi(argv[1]);
   size_t size = N * sizeof(float);
 
   // Allocate input vectors h_A and h_B in host memory
   float* h_A = (float*)malloc(size);
   float* h_B = (float*)malloc(size);
-
+  float* h_C = (float*)malloc(size);
+  
   // Initialize input vectors
-    ...
+  initarray(h_A, N, 10);
+  initarray(h_B, N, 10);
+
+  printarray(h_A, N);
+  printarray(h_B, N);
 
     // Allocate vectors in device memory
     float* d_A;
@@ -41,11 +67,16 @@ int main()
     // h_C contains the result in host memory
     cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
+    // print result
+    printarray(h_C, N);
+
     // Free device memory
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
             
     // Free host memory
-    ...
+    free(h_A);
+    free(h_B);
+    free(h_C);
       }
